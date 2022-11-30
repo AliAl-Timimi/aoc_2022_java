b0308fb759fb7d5292e324cbbef338cd0f6ec39a
Ali Al-Timimi <ali.al-timimi@student.kdg.be>
2022-11-30 19:51:32 +0100

    setup

---
+++ src/main/java/be/kdg/Main.java
@@ -0,0 +1,16 @@
+package be.kdg;
+
+import be.kdg.challenges.day1.Day1;
+
+// * This is the main class of the application.
+public class Main {
+    public static void main(String[] args) {
+        Day1.run();
+        // Day2.run();
+        // Day3.run();
+        // Day4.run();
+        // Day5.run();
+        // Day6.run();
+        // Day7.run();
+    }
+}
---
+++ src/main/java/be/kdg/challenges/day1/Day1.java
@@ -0,0 +1,7 @@
+package be.kdg.challenges.day1;
+
+public final class Day1 {
+    public static void run() {
+        System.out.println("Hello world!");
+    }
+}
